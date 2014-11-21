d259f78e605314ff875c6d57668b105253244028
Thomas Rouvinez <hadron.anomaly.dev@gmail.com>
2014-11-21 12:19:33 +0100

    Defined pass #1 service methods

--- WebService/HubNet/urls.py
+++ WebService/HubNet/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import patterns, url
-
 from HubNet import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
+	# General view, returns the version.
+    url(r'version', views.version, name='Version'),
+	
+	# Input record data view, returns success value.
+    url(r'irec$', views.input_record, name='Input Record'),
+	
+	# Output live feed for the required timestamp.
+    url(r'olvf$', views.output_getLiveUpdate, name='Output Live Feed'),
 )
--- WebService/HubNet/views.py
+++ WebService/HubNet/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
-def index(request):
-	return HttpResponse("Sample view")
+# Get the version of the webservice.
+def version(request):
+	return HttpResponse("v0.1A")
+
+# Post a new record for an event & sensor.
+def input_record(request):
+	return HttpResponse("Input recording")
+
+# Get updated live feed.
+def output_getLiveUpdate(request):
+	return HttpResponse("Live Feed")
